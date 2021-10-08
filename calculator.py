70113323f3b3d7fabd0af7920855e61f320f4ee2
Frujenga <90202360+Frujenga@users.noreply.github.com>
2021-10-08 15:59:44 +0200

    simple calculator app

--- calculator.py
+++ calculator.py
@@ -1,10 +1,22 @@
 print("Calculator application. Please give me the")
 print("1.operand (integer)")
-operand1 = int(input())
+text = input()
+while not text.isnumeric():
+    print("Bad input, again")
+    text = input()
+operand1 = int(text)
+
 print("operator (+ | - | * | /)")
-operator = input()
+text = input()
+while text not in ['+', '-', '*', '/']:
+    print("Bad input, again")
+    text = input()
+operator = text
 print("2.operand (integer)")
-operand2 = int(input())
+while not text.isnumeric():
+    print("Bad input, again")
+    text = input()
+operand2 = int(text)
 
 if operator == '+':
     result = operand1 + operand2
@@ -16,3 +28,4 @@ elif operator == '/':
     result = operand1 / operand2
 
 print(f'result: {result}')
+exit(0)
