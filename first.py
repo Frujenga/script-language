d92883aad7264c539b065afafe63650358220733
Frujenga <90202360+Frujenga@users.noreply.github.com>
2021-10-08 12:41:10 +0200

    Input, convert and format

--- first.py
+++ first.py
@@ -1,5 +1,7 @@
-value = 8
-print(value)
+print("Pls give me your name")
+name = input()
+print("Pls give me your age")
+age = int(input())
 
-asd = "hello"
-print(asd)
+print("Your name:{0}, age: {1}".format(name, age))
+print("Next year you will be {0} years old".format(age+1))
