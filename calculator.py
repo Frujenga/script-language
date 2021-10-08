479b45406df65b61570da43ccad442c454259bc4
Frujenga <90202360+Frujenga@users.noreply.github.com>
2021-10-08 16:07:28 +0200

    simple calculator app

--- calculator.py
+++ calculator.py
@@ -1,32 +1,40 @@
-print("Calculator application. Please give me the")
-print("1.operand (integer)")
-text = input()
-while not text.isnumeric():
-    print("Bad input, again")
+def ask():
+    print("Calculator application. Please give me the")
+    print("1.operand (integer)")
     text = input()
-operand1 = int(text)
+    while not text.isnumeric():
+        print("Bad input, again")
+        text = input()
+    operand1 = int(text)
 
-print("operator (+ | - | * | /)")
-text = input()
-while text not in ['+', '-', '*', '/']:
-    print("Bad input, again")
+    print("operator (+ | - | * | /)")
     text = input()
-operator = text
+    while text not in ['+', '-', '*', '/']:
+        print("Bad input, again")
+        text = input()
+    operator = text
 
-print("2.operand (integer)")
-while not text.isnumeric():
-    print("Bad input, again")
-    text = input()
-operand2 = int(text)
+    print("2.operand (integer)")
+    while not text.isnumeric():
+        print("Bad input, again")
+        text = input()
+    operand2 = int(text)
+    return operator, operand1, operand2
 
-if operator == '+':
-    result = operand1 + operand2
-elif operator == '-':
-    result = operand1 - operand2
-elif operator == '*':
-    result = operand1 * operand2
-elif operator == '/':
-    result = operand1 / operand2
 
-print(f'result: {result}')
+
+def calculate(operator, operand1, operand2):
+    if operator == '+':
+        result = operand1 + operand2
+    elif operator == '-':
+        result = operand1 - operand2
+    elif operator == '*':
+        result = operand1 * operand2
+    elif operator == '/':
+        result = operand1 / operand2
+
+        print(f'result: {result}')
+
+
+print(calculate(ask()))
 exit(0)
