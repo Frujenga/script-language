d0c745c776cc878cb3fb0d3d32d70f362d833d4a
Frujenga <90202360+Frujenga@users.noreply.github.com>
2021-10-08 17:06:58 +0200

    simple calculator app

--- calculator_arg.py
+++ calculator_arg.py
@@ -2,11 +2,18 @@ import sys
 
 import secret_logic
 
-sys.argv
 
-op1= int[sys.argv[1]]
-opr =sys.argv[2]
-op2= int[sys.argv[3]]
+def print_usage_and_exit():
+    print("calculator app. OPERAND OPERATOR OPERAND")
+    exit(-1)
+
+
+if len(sys.argv) != 4:
+    print_usage_and_exit()
+
+op1 = int[sys.argv[1]]
+opr = sys.argv[2]
+op2 = int[sys.argv[3]]
 
 rst = secret_logic.calculate(opr, op1, op2)
 print(f"Result {rst}")
