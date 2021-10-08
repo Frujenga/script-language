84ed9842097f8705b5578a45437c6a79ffe2d55b
Frujenga <90202360+Frujenga@users.noreply.github.com>
2021-10-08 16:21:56 +0200

    simple calculator app

---
+++ secret_logic.py
@@ -0,0 +1,11 @@
+def calculate(operator, operand1, operand2):
+    result = 0
+    if operator == '+':
+        result = operand1 + operand2
+    elif operator == '-':
+        result = operand1 - operand2
+    elif operator == '*':
+        result = operand1 * operand2
+    elif operator == '/':
+        result = operand1 / operand2
+    return result
